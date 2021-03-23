90a11fafcdb1850fb895ed5b4b0585fe44958b1b
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-23 07:16:03 +0200

    Add app_id & weather url

--- weather/views.py
+++ weather/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 
 
 def index(request):
+    app_id = "7b06996e671b504dcc4a6e2ed52111ed"
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=' + app_id
     return render(request, 'weather/index.html')

bc9a195f82c955f02b5fd6d542e86b3f749020a2
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-23 07:44:07 +0200

    Add JSON responce @ index

--- weather/views.py
+++ weather/views.py
@@ -6,9 +6,17 @@ from django.shortcuts import render
 
 def index(request):
     app_id = "7b06996e671b504dcc4a6e2ed52111ed"
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=' + app_id
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + app_id
     city = "London"
 
-    response = requests.get(url.format(city))
-    print(response.text)
+    response = requests.get(url.format(city)).json()
+    # print(response.text)
+
+    city_info = {
+        'city': city,
+        'temp': response['main']['temp'],
+        'feels_like': response['main']['feels_like'],
+        'clouds': response['clouds']['all'],
+        'icon': response['weather']['icon']
+    }
     return render(request, 'weather/index.html')

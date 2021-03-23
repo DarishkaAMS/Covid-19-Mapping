f8c24b061479a2f1114d8789d734f6215e1f64d9
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-23 13:58:28 +0200

    Fixing Errors @ index_view

--- weather/views.py
+++ weather/views.py
@@ -8,15 +8,15 @@ from .models import City
 def index_view(request):
     app_id = "7b06996e671b504dcc4a6e2ed52111ed"
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + app_id
-    city = "London"
 
     cities = City.objects.all()
 
     all_cities = []
 
     for city in cities:
-        response = requests.get(url.format(city)).json()
-
+        response = requests.get(url.format(city.name)).json()
+        # print('NAME', city.name)
+        # print("RESP", response['main']['temp']) #['main']['temp']
         city_info = {
             'city': city.name,
             'temp': response['main']['temp'],
